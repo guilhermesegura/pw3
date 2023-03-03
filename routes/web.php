3d1496a1388aa4c2d127a703540e376325cb699b
guilhermesegura <guilhermesegura26@gmail.com>
2023-03-02 22:22:19 -0300

    mvc

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,22 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Principal');
 });
 
 Route::get('/laboratorio', function () {
     return view('Laboratorio');
+});
+
+Route::get('/quemsomos', function () {
+    return view('QuemSomos');
+});
+
+Route::get('/contato', function () {
+    return view('Contato');
+});
+
+
+Route::get('/reclamacao', function () {
+    return view('Reclamacao');
 });
